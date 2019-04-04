26446d3105503d2c0169f7b6a0c699559e74be8f
smoky0408 <40046110+smoky0408@users.noreply.github.com>
2019-04-04 10:15:38 -0600

    changed motor controllers

--- src/main/include/subsystems/Lift.h
+++ src/main/include/subsystems/Lift.h
@@ -1,10 +1,9 @@
 #pragma once
 
-//Change Talon back to Victor when appropriate
 #include "Wiring.h"
 #include <frc/DigitalInput.h>
 #include <frc/SpeedControllerGroup.h>
-#include <frc/Talon.h>
+#include <frc/PWMVictorSPX.h>
 #include <frc/commands/Subsystem.h>
 
 class Lift : public frc::Subsystem
@@ -16,8 +15,8 @@ class Lift : public frc::Subsystem
   Lift();
 
  private:
-  frc::Talon left_victor{PWM::left_liftmotor};
-  frc::Talon right_victor{PWM::right_liftmotor};
+  frc::PWMVictorSPX left_victor{PWM::left_liftmotor};
+  frc::PWMVictorSPX right_victor{PWM::right_liftmotor};
   frc::SpeedControllerGroup victors{left_victor, right_victor};
   // These limit switches are normally closed
   frc::DigitalInput down_switch{DIO::down_liftswitch};
